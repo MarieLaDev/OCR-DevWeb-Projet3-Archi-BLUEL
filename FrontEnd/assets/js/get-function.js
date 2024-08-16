9c6fab76f884d0d517175ebcb4d21ad8de370998
MarieLaDev <marie.dehaullon@gmail.com>
2024-08-17 01:48:21 +0200

    Mise à jour fichiers branche portfolio

--- FrontEnd/assets/js/get-function.js
+++ FrontEnd/assets/js/get-function.js
@@ -9,29 +9,8 @@ export async function getDonnees(type) {
      // convertit la réponse en objet JavaScript
     const data = await reponse.json();
     
-     /* Si besoin de stocker dans le localStorage organiser le json en ligne avec : const valeurWorks = JSON.stringify(works); */
+     /* Si besoin de stocker dans le localStorage organiser l'objet en json avec : const valeurWorks = JSON.stringify(works); */
 
+     console.log(data);
      return data
-
  }
-
- /*// récupération des data éventuellement stockées dans le localStorage
-let works = window.localStorage.getItem("works");
-
-// si localStorage vide alors on fait appel à l'API
-if (works === null) {
-    const reponse = await fetch("http://localhost:5678/api/works");
-    // await pour attendre la réponse du serveur avant de continuer
-     
-    // tranformer projets(works) en json
-    works = await reponse.json();
-     
-    // Puis les transformer en json en ligne pour le stockage
-    const valeurWorks = JSON.stringify(works);
-
-    // Stocke les informations dans le localStorage
-     window.localStorage.setItem("works", valeurWorks);
-} else {
-    // sinon le localStorage n'est pas vide alors on transforme le localStorage en objets JavaScript avec parse (inverse de stringify) 
-    works = JSON.parse(works);
-} */
