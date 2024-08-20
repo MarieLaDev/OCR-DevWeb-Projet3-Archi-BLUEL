dbbd1b41ecd6989e5494a99d24301242bf4349a4
MarieLaDev <marie.dehaullon@gmail.com>
2024-08-20 17:17:44 +0200

    Gestion du clic sur le lien login

--- FrontEnd/assets/js/main.js
+++ FrontEnd/assets/js/main.js
@@ -1,8 +1,13 @@
 import { genererGalerie } from "./genererGalerie.js";
 import { genererBtnsFiltres } from "./genererBtnFiltres.js";
+import { genererFenetreLogin } from "./genererFenetreLogin.js";
+
 
 // Appeler la fonction pour générer la galerie
 genererGalerie();
 
 // Appeler la fonction pour générer les filtres
 genererBtnsFiltres();
+
+// Appeler la fonctoin pour générer les liens du menu
+genererFenetreLogin();
