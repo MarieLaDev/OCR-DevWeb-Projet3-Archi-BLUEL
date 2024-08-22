cc65d6f6fb00f418f2fd486907dc5f5c14a6d8a5
MarieLaDev <marie.dehaullon@gmail.com>
2024-08-22 17:56:20 +0200

    Correction, simplification de code js, modification des ID page login et CSS en conséquence - début js connexion

--- FrontEnd/assets/js/genererGalerie.js
+++ FrontEnd/assets/js/genererGalerie.js
@@ -1,35 +1,49 @@
 import { getAPI } from "./getAPI.js";
 
 export async function genererGalerie() {
-     // Lancement du GET dans l'API works await pour attendre la réponse + GET des catégories
-     const works = await getAPI('works');
-     
-     // Récupération de l'élément DOM de la galerie qui accueillera les "work(s)"
-     const gallery = document.querySelector(".gallery");
+     // Essayer de générer la gallerie ou générer l'erreur système
+     try {
+          // Lancement du GET dans l'API works await pour attendre la réponse
+          const works = await getAPI('works');
+          
+          console.log(works);
+          
+          // Récupération de l'élément DOM de la galerie qui accueillera les "work(s)"
+          const gallery = document.querySelector(".gallery");
 
-     for (let i = 0; i < works.length; i++) {
-          const work = works[i];
+          for (let work of works) {
+               // Création du conteneur pour l'éléments "work"
+               const contenerWork = document.createElement("figure");
+               // ajout des data-* à figure
+               contenerWork.id = work.id;
+               contenerWork.dataset.userid = work.userId;
+               contenerWork.dataset.categoryid = work.category.id;
+               contenerWork.dataset.categoyName = work.category.name;
 
-          // Création du conteneur pour les éléments du "work"
-          const contenerWork = document.createElement("figure");
+                              
+               // Création des éléments dans le conteneur "work"
+               // L'image avec son src et alt
+               const imgWork = document.createElement("img");
+               imgWork.src = work.imageUrl; // Ajout de lURL de l'image
+               imgWork.alt = work.title; // Ajout de la balise alt à l'image
+               
+               // Le titre du projet
+               const titleWork = document.createElement("figcaption");
+               // Utiliser textContent pour que le DOM "enregistre" l'élément quelque soit le style par exemple, avec un style "hidden" innerText le titleWork serait vide
+               titleWork.textContent = work.title;
 
-          // Création des éléments dans le conteneur "work"
-          const imgWork = document.createElement("img");
-          imgWork.src = work.imageUrl;
-          imgWork.setAttribute("alt", work.title); // Ajout de la balise alt à l'image
-          const titleWork = document.createElement("figcaption");
-          titleWork.innerText = work.title;
+               // Rattacher les éléments pour chaque "work"
+               contenerWork.appendChild(imgWork);
+               contenerWork.appendChild(titleWork);
 
-          // ajout des éléments
-          contenerWork.setAttribute("workid", work.id);
-          contenerWork.setAttribute("userid", work.userId);
-          contenerWork.setAttribute("categoryid", work.category.id)
-
-          // Rattacher les éléments pour chaque "work"
-          contenerWork.appendChild(imgWork);
-          contenerWork.appendChild(titleWork);
-
-          // Ajouter le conteneur à la galerie
-          gallery.appendChild(contenerWork);
+               // Ajouter le conteneur à la galerie
+               gallery.appendChild(contenerWork);
+          }
+          // Si la fonction échoue créer l'erreur
+          
+     } catch (error) {
+          // Afficher le message d'erreur avec alert pour récupérer l'erreur créée avec throw new Error il faut ${error.message} error seul ne suffit pas
+          alert(`Erreur : ${error.message}`);
      }
+
 }
--- FrontEnd/assets/js/gererFiltres.js
+++ FrontEnd/assets/js/gererFiltres.js
@@ -10,73 +10,77 @@ import { getAPI } from "./getAPI.js";
  * Gère les boutons filtres de la page index.html la variable btnsFiltres permettra de récupérer les catégories pour l'ajout de projets
  */
 export function gererFiltres() {
-     genererBtnsFiltres().then(() => {
-     
-          // Récupérer les éléments boutons
-          let btnsFiltres = document.querySelectorAll(".btn-zone a");
-          console.log(btnsFiltres);
-          btnsFiltres = Array.from(btnsFiltres);
-          console.log(btnsFiltres);
-     
-          // Pour parcourir les éléments d'une NodeList .forEach     
-          for (let filtre of btnsFiltres) {
-               // Pour chaque filtre du tableau btnsFiltes "écouter" le clic
-               filtre.addEventListener("click", (event) => {
-                    // récupérer le bouton cliqué
-                    const clickedBtn = event.target;
-                    console.log(clickedBtn);
-                    console.log("Le bouton cliqué est : " + clickedBtn.name + " et son ID est " + clickedBtn.id);
-                    
-                    clickedBtn.classList.add("focus"); 
-                    clickedBtn.classList.remove("filter-btn");
-                    
-                    // Retirer le focus des autres boutons
-                    for (let btn of btnsFiltres) {
-                         if (btn !== clickedBtn) {
-                              console.log("On change les class sur le bouton " + btn.name + " qui n'a pas été cliqué")
-                              btn.classList.add("filter-btn"); 
-                              btn.classList.remove("focus");     
-                         } 
-                    }
-                    
-                    // Récupérer les works dans le DOM
-                    const works = document.querySelectorAll(".gallery figure");
-                    
-                    // éviter le rechargement de la page pour que l'affichage ne bouge pas
-                    event.preventDefault();
-                    
-                    // Si bouton cliqué est "Tous"
-                    if (clickedBtn.id === "0") {
-                         // Pour chaque projet
-                         for (let work of works) {
-                              // Retirer la classe hidden 
-                              work.classList.remove("hidden");
-                              console.log("Le bouton " + clickedBtn.name + " a été cliqué on fait apparaitre tous les  projets")
+     try {
+          // générer d'abord les boutons avec la fonction genererBtnsFiltres puis...
+          genererBtnsFiltres().then(() => {
+               
+               // Récupérer les éléments boutons
+               let btnsFiltres = document.querySelectorAll(".btn-zone a");
+               // Transforme btnsFiltres en tableau
+               console.log(btnsFiltres);
+          
+               // Pour parcourir les éléments avec for of     
+               for (let filtre of btnsFiltres) {
+                    // Pour chaque filtre du tableau btnsFiltes "écouter" le clic
+                    filtre.addEventListener("click", (event) => {
+                         // récupérer le bouton cliqué
+                         const clickedBtn = event.target;
+                         console.log(clickedBtn);
+                         console.log("Le bouton cliqué est : " + clickedBtn.name + " et son ID est " + clickedBtn.id);
+
+                         // on ajoute la class "focus" & retire la class "filter-btn" sur le bouton cliqué
+                         clickedBtn.classList.add("focus"); 
+                         clickedBtn.classList.remove("filter-btn");
+                         console.log("On change les class sur le bouton " + clickedBtn.name + " qui a été cliqué")
+
+                         // Retirer le focus des autres boutons
+                         for (let btn of btnsFiltres) {
+                              if (btn !== clickedBtn) {
+                                   btn.classList.add("filter-btn"); 
+                                   btn.classList.remove("focus");     
+                              } 
                          }
-                    
-                    } else {
-                         for (let work of works) {
-                              // Si l'id du work est égal à l'id du bouton cliqué
-                              if (clickedBtn.id === work.getAttribute("categoryId")) {
-                                   // retirer la classe hidden au projet
+                         
+                         // Récupérer les works dans le DOM
+                         const works = document.querySelectorAll(".gallery figure");
+                         
+                         // éviter le rechargement de la page pour que l'affichage ne bouge pas
+                         event.preventDefault();
+                         
+                         // Si bouton cliqué est "Tous"
+                         if (clickedBtn.id === "0") {
+                              // Pour chaque projet
+                              for (let work of works) {
+                                   console.log(work);
+                                   // Retirer la classe hidden 
                                    work.classList.remove("hidden");
-                              } else {
-                                   // Ajouter la classe hidden au projet
-                                   work.classList.add("hidden");
+                                   console.log("Le bouton " + clickedBtn.name + " a été cliqué on fait apparaitre tous les projets");
+                              }
+                         
+                         } else {
+                              for (let work of works) {
+                                   console.log(work.dataset.categoryid);
+                                   // Si l'id du work est égal à l'id du bouton cliqué
+                                   if (clickedBtn.id === work.dataset.categoryid) {
+                                        // retirer la classe hidden au projet
+                                        work.classList.remove("hidden");
+                                   } else {
+                                        // Ajouter la classe hidden au projet
+                                        work.classList.add("hidden");
+                                   }
                               }
                          }
-                    }
-               });
-          }
-     });  
+                    });
+               }
+          });
+     } catch(erreur) {
+          alert(erreur.message);
+     }
+     
 }
 
 /**
  * Permet de créer les éléments DOM des filtres 
- * 
- * 
- * 
- * VOIR DATA à la place de setAttribute
  */
 async function genererBtnsFiltres() {
      const portfolio = document.getElementById("portfolio");
@@ -92,7 +96,6 @@ async function genererBtnsFiltres() {
 
      // Création d'un objet listeCategorie avec "Tous"
      let listeCategorie = [{"id" : 0, "name" : "Tous"},];
-     console.log(listeCategorie);
      
      // Récupère les catégories
      const categories = await getAPI("categories");
@@ -100,6 +103,11 @@ async function genererBtnsFiltres() {
      // Ajouter 1 par 1 les éléments
      listeCategorie.push(...categories);
      console.log(listeCategorie);
+     
+     // Stocker les catégories dans sessionStorage
+     sessionStorage.setItem("categories", JSON.stringify(categories));
+     console.log(`Les catégories de l'API sont stockées dans sessionStorage`);
+     console.log(`Les catégories stockées dans localSession sont ${sessionStorage.getItem("categories")}`);
 
      // Créer les boutons dans boucle for
      for (let categorie of listeCategorie) {
@@ -118,10 +126,11 @@ async function genererBtnsFiltres() {
           
           // Le texte dans la balise a element.textcontent = "..."
           genereFiltre.textContent = categorie.name;
-          genereFiltre.name = categorie.name;
-          genereFiltre.setAttribute("id", categorie.id )
+          genereFiltre.dataset.categorieName = categorie.name;
+          genereFiltre.id = categorie.id;
 
           // Mettre les btn filtres dans le contFiltres
           contFiltres.appendChild(genereFiltre);
      }
+     return categories;
 }
